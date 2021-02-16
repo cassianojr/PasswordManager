f8f878ff2bb4fc3f46c0da0244c19849abff5bbe
cassianojr <cassiano.jr.2009@hotmail.com>
2021-02-16 17:59:58 -0300

    Added UI for create password fragment

---
+++ app/src/main/java/br/com/cassianojunior/passwordmanager/ui/create_password/CreatePasswordViewModel.kt
@@ -0,0 +1,7 @@
+package br.com.cassianojunior.passwordmanager.ui.create_password
+
+import androidx.lifecycle.ViewModel
+
+class CreatePasswordViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
